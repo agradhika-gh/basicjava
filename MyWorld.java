afd2c2d51ed4b369eed82e8a4206d1d37c3874fb
unknown <agradhika@gmail.com>
2020-07-01 21:01:49 -0700

    'MyWorld.java MyWorld.java

---
+++ MyWorld.java
@@ -0,0 +1,22 @@
+
+public class MyWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "My Name is Radhika";
+		System.out.println("Name: " + name);
+		int a, b;
+		a = 100;
+		b = 100;
+		int x = a +b ;
+		System.out.println("Value of x: "+ x);
+		boolean flag;
+		
+		if (x == 200) {
+			flag = true;
+			System.out.println("Value of the flag: "+flag);
+		}
+
+	}
+
+}
