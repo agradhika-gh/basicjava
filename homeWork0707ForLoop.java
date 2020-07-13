2a747a167a089287102a3d9973ead6149057bac1
unknown <agradhika@gmail.com>
2020-07-12 23:21:15 -0700

    0707 Java homework

---
+++ homeWork0707ForLoop.java
@@ -0,0 +1,12 @@
+
+public class homeWork0707ForLoop {
+
+	public static void main(String[] args) {
+		
+		//Simple for loop
+		for (int x=1;x<=20;x++)
+			System.out.println(x);
+
+	}
+
+}
---
+++ print6TimesTable.java
@@ -0,0 +1,15 @@
+
+public class print6TimesTable {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int z;
+		int tnumber = 6;
+		for ( ; x<=10; x++) {
+			z = x * tnumber;
+			System.out.println("6 * "+ x + " = "+z);
+		}
+	
+	}
+
+}
