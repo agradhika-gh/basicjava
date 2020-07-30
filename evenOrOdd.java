b05fab72664f66e6c1cd3600b4c763809bdf20fd
unknown <agradhika@gmail.com>
2020-07-29 23:29:29 -0700

    EvenorOdd Java

---
+++ evenOrOdd.java
@@ -0,0 +1,13 @@
+public class evenOrOdd {
+
+	public static void main(String[] args) {
+
+		int number = 13;
+		
+		if (number % 2 == 0) {
+			System.out.println("Number is Even");
+		} else {
+			System.out.println("Number is Odd");
+		}
+	}
+}
