fcea23cbfa253a1b00de63164909323a82145887
unknown <agradhika@gmail.com>
2020-08-02 22:38:08 -0700

    My FirstOOP project

---
+++ MyFirstOOP/src/Application.java
@@ -0,0 +1,15 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Person person1 = new Person();
+		person1.name = "Person1";
+		person1.age = 19;
+		
+		Person person2 = new Person();
+		person2.name = "Another Person";
+		person2.age = 21;
+		
+	}
+}
